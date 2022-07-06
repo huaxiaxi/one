8d5d922645cda4665ee955ed6cbeccef9a10bc92
huaxiaxi <68362444+huaxiaxi@users.noreply.github.com>
2022-07-06 18:17:59 +0800

    startListNodeV1

---
+++ src/start/A02_linkedlist/B001_delete_linkedlist_element.java
@@ -0,0 +1,109 @@
+package start.A02_linkedlist;
+
+import java.util.Scanner;
+
+public class B001_delete_linkedlist_element {
+    public static void main(String[] args) {
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("请输入链表：");
+            String scIn = sc.nextLine();
+            Integer target = sc.nextInt();
+            if (scIn.length() == 0 || scIn.isBlank()) {
+                System.out.println(-1);
+                return;
+            }
+            String[] arrStr = scIn.split(" ");
+            int[] arr = new int[arrStr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = Integer.parseInt(arrStr[i]);
+            }
+            ListNode head = arrayTransLinkedList(arr);
+
+//            ListNode result = removeListNodeElement1(head, target);
+//            ListNode result = removeListNodeElement2(head, target);
+            ListNode result = removeListNodeElement3(head, target);
+
+            printListNode(result);
+        }
+    }
+//    添加虚拟节点
+    private static ListNode removeListNodeElement1(ListNode head, Integer target) {
+        if (head == null){
+            return head;
+        }
+        ListNode dummpy = new ListNode(-1,head);
+        ListNode pre = dummpy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val == target) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummpy.next;
+    }
+//    不添加虚拟节点
+    private static ListNode removeListNodeElement2(ListNode head, Integer target) {
+        while (head != null && head.val == target) {
+            head = head.next;
+        }
+        if (head == null){
+            return head;
+        }
+        ListNode pre = head;
+        ListNode cur = head.next;
+        while (cur != null) {
+            if (cur.val == target) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+//    不添加虚拟节点且仅使用一个节点
+    private static ListNode removeListNodeElement3(ListNode head, Integer target) {
+        while (head != null && head.val == target) {
+            head = head.next;
+        }
+        if (head == null){
+            return head;
+        }
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.next.val == target) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+    public static void printListNode(ListNode head) {
+        if (head == null){
+            System.out.println(-1);
+            return;
+        }
+        while (head != null){
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static ListNode arrayTransLinkedList(int[] arr) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode next = head;
+        for (int i = 1; i < arr.length; i++) {
+            ListNode node = new ListNode(arr[i]);
+            next.next = node;
+            next = node;
+        }
+        return head;
+    }
+
+
+}
